49bc94a9d640e23bbeb5fee06711fe44949a77ea
Adnan Al Zahabi <99538471+zadnan2002@users.noreply.github.com>
2023-06-03 16:21:58 +0300

    Add sitemap

---
+++ src/generate-sitemap.js
@@ -0,0 +1,16 @@
+const sitemap=require('sitemap');
+const fs=require('fs');
+const hostname='https://addyscorner.tech';
+
+const urls=[
+    {url: '/', changefreq: 'daily', priority: 1},
+    {url: '/about', changefreq: 'daily', priority: 0.5},
+
+]
+
+const sitemapInstance=sitemap.createSitemap({
+    hostname: hostname,
+    urls: urls,
+});
+
+fs.writeFileSync('./sitemap.xml', sitemapInstance.toString());
