8997e4bf5470843801574c305b1d749e4866fb80
Adnan Al Zahabi <99538471+zadnan2002@users.noreply.github.com>
2023-09-26 18:06:53 +0300

    Add Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
+import "./App.scss";
 
-import './App.scss';
-
-import ResponsiveAppBar from './components/Sidebar/Sidebar';
-import Home1 from './Home1';
-import Home2 from './Home2';
-import Home3 from './Home3';
-import HomeSnow from './HomeSnow';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ReactGA from 'react-ga';
-import { useEffect } from 'react';
-import Error404 from './Error404';
-
-
-
-
-
+import ResponsiveAppBar from "./components/Sidebar/Sidebar";
+import Home1 from "./Home1";
+import Home2 from "./Home2";
+import Home3 from "./Home3";
+import HomeSnow from "./HomeSnow";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+import { Analytics } from "@vercel/analytics";
+import Error404 from "./Error404";
 
 function App() {
-  ReactGA.initialize('G-NBFCWVE4LG');
+  ReactGA.initialize("G-NBFCWVE4LG");
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
-
   });
 
   return (
-
     <Router>
       <div className="App">
         <HomeSnow />
+        <Analytics></Analytics>
         <Switch>
           <Route exact path="/" component={Home1} />
           <Route path="/about" component={Home3} />
